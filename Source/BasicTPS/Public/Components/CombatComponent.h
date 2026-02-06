60ca32c1846921d9ed416480e9b7fdb78029143d
h-h4 <raphi331110@gmail.com>
2026-02-06 16:28:42 +0900

    create combatcomponent

---
+++ Source/BasicTPS/Public/Components/CombatComponent.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "CombatComponent.generated.h"
+
+class UStatComponent;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class BASICTPS_API UCombatComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UCombatComponent();
+	
+protected:
+	virtual void BeginPlay() override;
+	
+public:
+	void ExecuteAttack(AActor* TargetActor, float DamageAmount, FHitResult HitInfo);
+	void OnTakeDamage(float IncomingDamage);
+	
+private:
+	UPROPERTY()
+	TObjectPtr<UStatComponent> StatComp;
+};
