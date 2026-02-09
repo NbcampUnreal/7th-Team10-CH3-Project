df0fac70f58d37d8366846b988d746118ecc84f6
h-h4 <raphi331110@gmail.com>
2026-02-09 21:18:58 +0900

    스탯 컴포넌트 생성 및 구현

---
+++ Source/BasicTPS/Private/03_Components/BTPS_StatComponent.cpp
@@ -0,0 +1,105 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "03_Components/BTPS_StatComponent.h"
+
+
+UBTPS_StatComponent::UBTPS_StatComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bStartWithTickEnabled = false;
+	bWantsInitializeComponent = true;
+}
+
+void UBTPS_StatComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	CurrentHP = MaxHP;
+	CurrentStamina = MaxStamina;
+}
+
+void UBTPS_StatComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+	RecoverStamina(StaminaRegenRate * DeltaTime);
+	if (CurrentStamina >= MaxStamina)
+	{
+		SetComponentTickEnabled(false);
+	}
+}
+
+
+void UBTPS_StatComponent::OnTakeDamage(float DamageAmount)
+{
+	CurrentHP = FMath::Clamp(CurrentHP - DamageAmount, 0.0f, MaxHP);
+	if (OnHPChanged.IsBound())
+	{
+		OnHPChanged.Broadcast(CurrentHP, MaxHP);
+	}
+	
+	if (CurrentHP <= KINDA_SMALL_NUMBER)
+	{
+		if (OnDeath.IsBound())
+		{
+			OnDeath.Broadcast();
+		}
+	}
+}
+
+void UBTPS_StatComponent::Heal(float HealAmount)
+{
+	CurrentHP = FMath::Clamp(CurrentHP + HealAmount, 0.0f, MaxHP);
+	if (OnHPChanged.IsBound())
+	{
+		OnHPChanged.Broadcast(CurrentHP, MaxHP);
+	}
+}
+
+bool UBTPS_StatComponent::bUseStamina(float Cost)
+{
+	if (CurrentStamina < Cost)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Not Enough Stamina!"));
+		return false;
+	}
+	
+	CurrentStamina = FMath::Clamp(CurrentStamina - Cost, 0.0f, MaxStamina);
+	StartRegenTimer();
+	
+	if (OnStaminaChanged.IsBound())
+	{
+		OnStaminaChanged.Broadcast(CurrentStamina, MaxStamina);
+	}
+	
+	return true;
+}
+
+void UBTPS_StatComponent::RegenStamina()
+{	
+	SetComponentTickEnabled(true);
+}
+
+void UBTPS_StatComponent::RecoverStamina(float RecoverAmount)
+{
+	CurrentStamina = FMath::Clamp(CurrentStamina + RecoverAmount, 0.0f, MaxStamina);
+	if (OnStaminaChanged.IsBound())
+	{
+		OnStaminaChanged.Broadcast(CurrentStamina, MaxStamina);
+	}
+}
+
+void UBTPS_StatComponent::StartRegenTimer()
+{
+	SetComponentTickEnabled(false);
+
+	GetWorld()->GetTimerManager().SetTimer(
+		StaminaRegenTimerHandle,
+		this,
+		&UBTPS_StatComponent::RegenStamina,
+		StaminaRegenDelay,
+		false
+	);
+}
+
+
---
+++ Source/BasicTPS/Public/03_Components/BTPS_StatComponent.h
@@ -0,0 +1,91 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "BTPS_StatComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnHPChangedDelegate, float, CurrentHP, float, MaxHP);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnStaminaChangedDelegate, float, CurrentStamina, float, MaxStamina);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDeathDelegate);
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class BASICTPS_API UBTPS_StatComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UBTPS_StatComponent();
+	
+protected:
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	
+public:
+	
+	UPROPERTY(BlueprintAssignable, Category = "Stat")
+	FOnHPChangedDelegate OnHPChanged;
+	
+	UPROPERTY(BlueprintAssignable, Category = "Stat")
+	FOnStaminaChangedDelegate OnStaminaChanged;
+	
+	UPROPERTY(BlueprintAssignable, Category = "Stat")
+	FOnDeathDelegate OnDeath;
+
+	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stat|Health")
+	float MaxHP = 100.0f;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Stat|Health")
+	float CurrentHP;
+	
+public:	
+	UFUNCTION(BlueprintPure, Category = "Stat|Health")
+	float GetMaxHP() const {return MaxHP;}
+	
+	UFUNCTION(BlueprintPure, Category = "Stat|Health")
+	float GetCurrentHP() const {return CurrentHP;}
+	
+	UFUNCTION(BlueprintCallable, Category = "Stat|Health")
+	void OnTakeDamage(float DamageAmount);
+	
+	UFUNCTION(BlueprintCallable, Category = "Stat|Health")
+	void Heal(float HealAmount);
+	
+	
+	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stat|Stamina")
+	float MaxStamina = 100.0f;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Stat|Stamina")
+	float CurrentStamina;
+	
+	UPROPERTY(EditAnywhere, Category = "Stat|Stamina")
+	float StaminaRegenRate = 5.0f;
+	
+	UPROPERTY(EditAnywhere, Category = "Stat|Stamina")
+	float StaminaRegenDelay = 1.0f;
+	
+	FTimerHandle StaminaRegenTimerHandle;
+	
+public:	
+	UFUNCTION(BlueprintPure, Category = "Stat|Stamina")
+	float GetMaxStamina() const {return MaxStamina;}
+	
+	UFUNCTION(BlueprintPure, Category = "Stat|Stamina")
+	float GetCurrentStamina() const {return CurrentStamina;}
+	
+	UFUNCTION(BlueprintCallable, Category = "Stat|Stamina")
+	bool bUseStamina(float Cost);
+	
+	void RegenStamina();
+	void RecoverStamina(float RecoverAmount);
+
+private:
+	float LastStaminaUseTime;
+	void StartRegenTimer();
+};
