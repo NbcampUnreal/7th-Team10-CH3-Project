8f8fa43bd2a6464b29acc92ae8dda7e51ba0b443
h-h4 <raphi331110@gmail.com>
2026-02-10 13:41:38 +0900

    fix 기초 파일 생성 및 Build.cs 수정
    0, 4, 5, 6, 7번 폴더와 그 내부에
    기초파일을 생성하고
    Build.cs 내부에
    "UMG"
    "AIModule",
    "NavigationSystem",
    "GameplayTasks"
    를 추가시켰습니다.

---
+++ Source/BasicTPS/Private/04_Items/BTPS_ItemBase.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "04_Items/BTPS_ItemBase.h"
+
+
+// Sets default values
+ABTPS_ItemBase::ABTPS_ItemBase()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ABTPS_ItemBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABTPS_ItemBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
