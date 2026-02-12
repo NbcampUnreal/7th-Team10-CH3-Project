a78401ebc63499b2fda59a530644493e855ebc7d
h-h4 <raphi331110@gmail.com>
2026-02-12 20:06:51 +0900

    add: AI컨트롤러 구현

--- Source/BasicTPS/Private/06_Ai/BTPS_AIController.cpp
+++ Source/BasicTPS/Private/06_Ai/BTPS_AIController.cpp
@@ -2,25 +2,54 @@
 
 
 #include "06_Ai/BTPS_AIController.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
 
-
-// Sets default values
 ABTPS_AIController::ABTPS_AIController()
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-}
-
-// Called when the game starts or when spawned
-void ABTPS_AIController::BeginPlay()
-{
-	Super::BeginPlay();
+	PrimaryActorTick.bCanEverTick = false;
 	
+	PerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("PerceptionComponent"));
+	SightConfig = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("SightConfig"));
+	
+	if (SightConfig)
+	{
+		SightConfig->SightRadius = 100.0f;
+		SightConfig->LoseSightRadius = 200.0f;
+		SightConfig->PeripheralVisionAngleDegrees = 90.0f;
+		
+		SightConfig->DetectionByAffiliation.bDetectEnemies = true;
+		SightConfig->DetectionByAffiliation.bDetectNeutrals = true;
+		SightConfig->DetectionByAffiliation.bDetectFriendlies = true;
+		
+		PerceptionComponent->ConfigureSense((*SightConfig));
+		PerceptionComponent->SetDominantSense((SightConfig->GetSenseImplementation()));
+		
+	}
 }
 
-// Called every frame
-void ABTPS_AIController::Tick(float DeltaTime)
+void ABTPS_AIController::OnPossess(APawn* InPawn)
 {
-	Super::Tick(DeltaTime);
+	Super::OnPossess(InPawn);
+	
+	if (PerceptionComponent)
+	{
+		PerceptionComponent->OnTargetPerceptionUpdated.AddDynamic(this, &ABTPS_AIController::OnTargetDetected);
+	}
 }
+
+void ABTPS_AIController::OnTargetDetected(AActor* Actor, FAIStimulus Stimulus)
+{
+	if (Actor && Stimulus.WasSuccessfullySensed())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI Controller: I see you! Target: %s"), *Actor->GetName());
+        
+		// TODO/hyun 여기에 나중에 공격 로직을 넣을 예정
+	}
+	else
+	{
+		UE_LOG(LogTemp, Log, TEXT("AI Controller: Target Lost: %s"), *Actor->GetName());
+	}
+}
+
 
