086058ce0e23af52a728777cf5df41d77a900865
h-h4 <raphi331110@gmail.com>
2026-02-19 20:39:16 +0900

    feat: 닿을시 데미지를 입는 컴포넌트 생성 및 구현

---
+++ Source/BasicTPS/Private/03_Components/BTPS_TouchDamageComponent.cpp
@@ -0,0 +1,78 @@
+﻿
+#include "03_Components/BTPS_TouchDamageComponent.h"
+#include "03_Components/BTPS_StatComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "GenericTeamAgentInterface.h"
+
+
+UBTPS_TouchDamageComponent::UBTPS_TouchDamageComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	
+	InitSphereRadius(60.0f);
+	SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	SetCollisionResponseToAllChannels(ECR_Ignore);
+	SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+}
+
+void UBTPS_TouchDamageComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	OnComponentBeginOverlap.AddDynamic(this, &UBTPS_TouchDamageComponent::OnTouchOverlap);
+	
+	AActor* MyOwner = GetOwner();
+	if (MyOwner)
+	{
+		UBTPS_StatComponent* OwnerStatComp = 
+			MyOwner->FindComponentByClass<UBTPS_StatComponent>();
+		
+		if (OwnerStatComp)
+		{
+			OwnerStatComp->OnDeath.AddDynamic
+			(this, &UBTPS_TouchDamageComponent::OnOwnerDeath);
+		}
+	}
+}
+
+void UBTPS_TouchDamageComponent::OnOwnerDeath()
+{
+	SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+
+void UBTPS_TouchDamageComponent::OnTouchOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	AActor* MyOwner = GetOwner();
+	
+	if (!MyOwner || !OtherActor || OtherActor == MyOwner) return;
+	
+	IGenericTeamAgentInterface* MyTeamAgent = Cast<IGenericTeamAgentInterface>(MyOwner);
+	IGenericTeamAgentInterface* OtherTeamAgent = Cast<IGenericTeamAgentInterface>(OtherActor);
+
+	bool bCanDamage = true;
+	
+	if (MyTeamAgent && OtherTeamAgent)
+	{
+		if (MyTeamAgent->GetGenericTeamId() == OtherTeamAgent->GetGenericTeamId())
+		{
+			bCanDamage = false;
+		}
+	}
+
+	if (bCanDamage)
+	{
+		APawn* OwnerPawn = Cast<APawn>(MyOwner);
+		AController* InstigatorCtrl = OwnerPawn ? OwnerPawn->GetController() : nullptr;
+		
+		UGameplayStatics::ApplyDamage(
+			OtherActor, 
+			DamageAmount, 
+			InstigatorCtrl, 
+			MyOwner, 
+			UDamageType::StaticClass()
+		);
+
+		UE_LOG(LogTemp, Warning, TEXT("[TouchDamage] %s dealt %f damage to %s!"), *MyOwner->GetName(), DamageAmount, *OtherActor->GetName());
+	}
+}
