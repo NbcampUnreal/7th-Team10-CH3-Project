02ef9f32794efd08497149ac3c141537d7450830
saltlake00 <saltlake5470@gmail.com>
2026-02-26 14:04:35 +0900

    feat: update gameplay hud stat

--- Source/BasicTPS/Private/05_UI/BTPS_MainWidget.cpp
+++ Source/BasicTPS/Private/05_UI/BTPS_MainWidget.cpp
@@ -2,6 +2,8 @@
 
 
 #include "05_UI/BTPS_MainWidget.h"
+
+#include "01_Game/BTPS_GameState.h"
 #include "03_Components/BTPS_StatComponent.h"
 #include "03_Components/BTPS_ShootingMachineComponent.h"
 #include "04_Items/A_Equipment/Weapon/BTPS_WeaponBase.h"
@@ -14,6 +16,31 @@
 void UBTPS_MainWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
+	
+	GetWorld()->GetTimerManager().SetTimer(
+	RetryBindHandle,
+	this,
+	&UBTPS_MainWidget::TickReadTime,
+	0.1f,
+	true
+);
+}
+
+void UBTPS_MainWidget::NativeDestruct()
+{
+	if (GetWorld())
+	{
+		GetWorld()->GetTimerManager().ClearTimer(RetryBindHandle);
+	}
+	Super::NativeDestruct();
+}
+
+void UBTPS_MainWidget::TickReadTime()
+{
+	auto* GS = GetWorld() ? GetWorld()->GetGameState<ABTPS_GameState>() : nullptr;
+	if (!GS) return;
+
+	UpdateLevelDuration();
 }
 
 void UBTPS_MainWidget::BindStatComp(UBTPS_StatComponent* StatComp)
@@ -38,6 +65,17 @@ void UBTPS_MainWidget::UpdateHP(float CurrentHP, float MaxHP)
 			HPBar->SetPercent(Percent);
 		}
 	}
+	
+	if (HPText)
+	{
+		if (MaxHP > 0.0f)
+		{
+			float Percent = CurrentHP / MaxHP;
+			HPText->SetText(FText::FromString(FString::Printf(TEXT("%.0f / %.0f"), CurrentHP, MaxHP)));
+
+		}
+	}
+	
 }
 
 void UBTPS_MainWidget::UpdateStamina(float CurrentStamina, float MaxStamina)
@@ -48,6 +86,16 @@ void UBTPS_MainWidget::UpdateStamina(float CurrentStamina, float MaxStamina)
 		{
 			float Percent = CurrentStamina / MaxStamina;
 			StaminaBar->SetPercent(Percent);
+		}
+	}
+	
+	if (StaminaText)
+	{
+		if (MaxStamina > 0.0f)
+		{
+			float Percent =  CurrentStamina / MaxStamina;
+			StaminaText->SetText(FText::FromString(FString::Printf(TEXT("%.0f / %.0f"), CurrentStamina, MaxStamina)));
+
 		}
 	}
 }
@@ -81,4 +129,16 @@ void UBTPS_MainWidget::UpdateAmmo(int32 CurrentAmmo, int32 MaxAmmo)
 
 		Text_AmmoInfo->SetVisibility(ESlateVisibility::Visible);
 	}
+}
+
+void UBTPS_MainWidget::UpdateLevelDuration()
+{
+	auto* GS = GetWorld() ? GetWorld()->GetGameState<ABTPS_GameState>() : nullptr;
+	if (!GS) return;
+
+	if (!IsValid(Timer)) return;
+
+	const float RemainingTime = GS->GetLevelRemainingTime();
+
+	Timer->SetText(FText::FromString(FString::Printf(TEXT("Time: %.1f"), RemainingTime)));
 }
